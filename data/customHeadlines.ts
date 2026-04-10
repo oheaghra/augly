0e028f28fb30cbb138056722cf25fa2f1122ac6e
oheaghra <oheaghra@yahoo.com>
2026-04-09 20:19:35 -0400

    changed headline

--- data/customHeadlines.ts
+++ data/customHeadlines.ts
@@ -5,7 +5,8 @@ export const customHeadlines: Record<string, string> = {
   
   "Richmond County considers big tax hike": "CSRA Homeowners Could See 18% Property Tax Increase Next Year",
   "Augusta Commissioners vote on new budget": "Augusta City Commission Just Approved a Controversial New Budget",
-  
+  "First Tee Augusta girls experience the magic of the Masters": "Girls annoy golfers"
+
   // Add more as you go...
 };
 
