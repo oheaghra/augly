5c878b69ec34870f173900c35c43a9be04bd8922
oheaghra <oheaghra@yahoo.com>
2026-04-10 19:23:24 -0400

    Trying to fix original article

--- data/originalArticles.ts
+++ data/originalArticles.ts
@@ -3,11 +3,11 @@ import { Article } from '../types';
 
 export const originalArticles: Article[] = [
   {
-    title: "Phinizy Swamp welcomes Spring",
+    title: "Phinizy Swamp open for visitors and plans for growth",
     link: "https://docs.google.com/document/d/1gYBJ7rsRIUZLabANqzT1ATYv0h0xAEzWOV6RFYFF5ag/edit?usp=sharing",   // ← Paste your Google Docs link here
     pubDate: "2026-04-09",
     source: "Augly Original",
-    description: "Phinizy Swamp gets busy",
+    description: "Sprawling over 1100 acres between Augusta Regional Airport and the Savannah River are 500 alligators and over 240 species of birds including bald eagles.",
     // image: "https://picsum.photos/id/1015/1200/630",   // Change this to a real image later
     category: "Politics",
   },
