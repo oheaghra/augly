0847b8f5a050097807e268085b962ebd26a648a9
oheaghra <oheaghra@yahoo.com>
2026-04-12 04:59:43 -0400

    updated featured articles

--- data/featuredArticles.ts
+++ data/featuredArticles.ts
@@ -3,7 +3,7 @@
 // Add full article URLs here to promote them to the top full-size section
 export const featuredLinks = new Set<string>([
   // Example:
-  // "https://theaugustapress.com/some-article-title/",
+  "https://www.wjbf.com/news/crime-news/child-murder-suspect-apprehended-after-hours-long-standoff-rcso-says/",
   
   // ← Paste your promoted links here (with quotes)
 ]);
